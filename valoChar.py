1be711ef6d6f46fa4ae3b57ec5501d561cdef438
jasmgang <jasmin@fuglsangkristensen.dk>
2021-04-21 13:55:25 +0200

    gider du ikke selv vælge karakter i valorant, så brug dette script! Hvis du kan lide Sage altså

--- valoChar.py
+++ valoChar.py
@@ -1,14 +1,20 @@
 import random
-karakter = ["Sage", "Killjoy", "Viper", "Cypher"]
+from rich.console import Console
+
+console = Console()
+
+karakter = ['Astra', 'Breach', 'Brimstone', 'Cypher', 'Jett', 'Killjoy', 'Omen', 'Phoenix', 'Raze', 'Reyna', 'Sage',
+            'Skye', 'Viper', 'Yoru']
 
 
 def char(c):
-    if "ja" in c or "j" in c or "" in c:
-        return random.choices(karakter, weights=(50, 20, 10, 10), k=1)[0] + "\n\n"
+    if "ja" in c or c == "j" or c == "":
+        return f"Du skal spille" \
+            f" [yellow]{random.choices(karakter, weights=(3, 2, 2, 10, 1, 20, 2, 2, 1, 1, 50, 5, 10, 1), k=1)[0]}[/yellow] i den næste kamp\n\n"
     else:
         return "Så gå et andet sted hen, tak.\n\n"
 
 
 while True:
     valg = input("Har du svært ved at vælge karakter? \n").lower()
-    print(char(valg))
+    console.print(char(valg))
