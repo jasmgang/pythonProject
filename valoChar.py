72e0059d9dfc7aabf64ad38d2685da8a09c35e8a
jasmgang <jasmin@fuglsangkristensen.dk>
2021-04-21 12:01:05 +0200

    et perfekt script til at vælge din karakter i valo

---
+++ valoChar.py
@@ -0,0 +1,12 @@
+import random
+karakter = ["Sage", "Killjoy", "Viper", "Cypher" ]
+
+def Char(c):
+    if "ja" in c or "j" in c or "" in c:
+        return random.choices(karakter, weights=(50, 20, 10, 10), k=1)[0] + "\n\n"
+    else:
+        return "Så gå et andet sted hen, tak.\n\n"
+
+while True:
+    valg = input("Har du svært ved at vælge karakter? \n").lower()
+    print(Char(valg))
