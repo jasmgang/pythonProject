4edf6575f16083c20304a20508e552ac51ee3b6e
DNIIBOY <32793938+DNIIBOY@users.noreply.github.com>
2021-11-17 11:21:56 +0100

    nye random weights

--- valoChar.py
+++ valoChar.py
@@ -10,7 +10,7 @@ karakter = ['Astra', 'Breach', 'Brimstone', 'Cypher', 'Jett', 'Killjoy', 'Omen',
 def char(c):
     if "ja" in c or c == "j" or c == "":
         return f"Du skal spille" \
-            f" [yellow]{random.choices(karakter, weights=(5, 2, 2, 12, 1, 20, 4, 2, 1, 1, 50, 5, 8, 1), k=1)[0]}[/yellow] i den næste kamp\n\n"
+            f" [yellow]{random.choices(karakter, weights=(20, 2, 3, 12, 1, 40, 3, 2, 1, 1, 15, 12, 25, 1), k=1)[0]}[/yellow] i den næste kamp\n\n"
     else:
         return "Så gå et andet sted hen, tak.\n\n"
 
